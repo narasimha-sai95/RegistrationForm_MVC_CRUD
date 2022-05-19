c69e08764d97bf2b96e2c1da39b0865347810e18
NarasimhaSai <180030334cse@gmail.com>
2022-05-19 14:58:37 +0530

    Add project files.

---
+++ final/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using final.Models;
+
+namespace final.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Details e)
+        {
+            if (ModelState.IsValid)
+            {
+                RegistrationEntities1 db=new RegistrationEntities1();
+                db.Details.Add(e);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(e);
+        }
+    }
+}
---
+++ final/Models/emp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace final.Models
+{
+    public class emp
+    {
+
+        public int id { get; set; }
+        
+        public string Name { get; set; }
+       
+        public string MobileNumber { get; set; }
+
+       
+        public string Email { get; set; }
+
+        public string Designation { get; set; }
+
+        public string Department { get; set; }
+
+        public string OfficeLocation { get; set; }
+    }
+}
